b5b696bebb35bb7559359601d4c2a559805ba590
Xenobius1382 <shubinmax13@gmail.com>
2024-06-21 00:08:19 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,40 @@
+#Задача "длина слова"
+
+a = "lesson"
+print(len(a))
+
+#Задача "суммы и разности"
+
+first = 5
+second = 8
+summa = (first + second) #сумма чисел
+diff = (first - second) #разность чисел
+print(summa)
+print(diff)
+
+#Задача "среднее арифметическое
+
+first = 3
+second = 7
+third = 9
+mean = ((first + second + third)/3) #среднее арифметическое трех чисел
+print(mean)
+
+#Задача "простые строчки"
+
+first_string = "Вторник"
+second_string = "Понедельник"
+print(second_string + ", " + first_string)
+
+#Задача "сложная формула
+
+a = 5
+b = 11
+c = 24
+f = (((a * b) + (a * c)) ** 3)/2
+print(f)
+
+
+
+
+
